3b3b0e45ffb954a0dab4937621b04541345a4fcd
fabiogermana <fabio.germana@igesa.it>
2023-05-18 12:44:48 +0200

    approve ps

--- script/index2.js
+++ script/index2.js
@@ -1,85 +1,87 @@
-$(document).ready(()=>{
-    var user = Cookies.get("user");
-    $.get("/setting?user="+user,function(dati){
-        dati=JSON.parse(dati)
-        if(dati.intro!="S"){
-         location.href="/pages/login.html"
-        }
-    })
-})
-var i =0
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
+$(document).ready(() => {
+  var user = Cookies.get("user");
+  $.get("/setting?user=" + user, function (dati) {
+    dati = JSON.parse(dati);
+    if (dati.intro != "S") {
+      location.href = "/pages/login.html";
+    }
+  });
+});
+var i = 0;
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
 };
 
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
-    if (this.isDeleting) {
+  if (this.isDeleting) {
     this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
+  } else {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+  }
 
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
-    var that = this;
-    var delta = 200 - Math.random() * 100;
+  var that = this;
+  var delta = 200 - Math.random() * 100;
 
-    if (this.isDeleting) { delta /= 2; }
-    
-    if (!this.isDeleting && this.txt === fullTxt) {
-    i++
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    i++;
     delta = this.period;
     this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
+  } else if (this.isDeleting && this.txt === "") {
     this.isDeleting = false;
     this.loopNum++;
     delta = 500;
-    }
-  
- 
-    setTimeout(function() {
-        if(i!=2){
-            that.tick();
-        }else{
-            sleep(1500)
-            console.log(navigator.userAgent)
-            if(navigator.userAgent.includes("gonative") && screen.height < screen.width){
-                location.href = "/pages/activate.html";
-              }else{
-                location.href="/pages/login.html"
-              }
-           
-        }
+  }
 
-    }, delta);
+  setTimeout(function () {
+    if (i != 2) {
+      that.tick();
+    } else {
+      sleep(1500);
+      console.log(navigator.userAgent);
+      if (
+        navigator.userAgent.includes("gonative") &&
+        screen.height < screen.width
+      ) {
+        //location.href = "/pages/activate.html";
+        location.href = "/pages/login.html";
+      } else {
+        location.href = "/pages/login.html";
+      }
+    }
+  }, delta);
 };
 function sleep(miliseconds) {
-    var currentTime = new Date().getTime();
-    while (currentTime + miliseconds >= new Date().getTime()) {
-    }
+  var currentTime = new Date().getTime();
+  while (currentTime + miliseconds >= new Date().getTime()) {}
 }
 
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
     }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
 };
